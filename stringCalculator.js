cb179a89c97fd4b9fe717a5fd95e4e82b609a3c9
chaitali.gawande <chaitali.gawande@jadeglobal.com>
2025-01-17 11:11:58 +0530

    new line character support added

--- stringCalculator.js
+++ stringCalculator.js
@@ -5,6 +5,7 @@
 // Input: “”, Output: 0
 // Input: “1”, Output: 1
 // Input: “1,5”, Output: 6
+// Input: "1\n2,3", Output:6
 
 function add(numbers) {
     // Handle the empty string case
@@ -12,8 +13,8 @@ function add(numbers) {
         return 0;
     }
     
-    // Split the input string by commas and convert each part to a number
-    const numberArray = numbers.split(',').map(Number);
+    // Replace newlines (\n) with commas and split the string by commas
+    const numberArray = numbers.replace(/\n/g, ',').split(',').map(Number);
     
     // Sum up the numbers in the array
     return numberArray.reduce((sum, num) => sum + num, 0);
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -16,4 +16,10 @@ describe('String Calculator', () => {
   test('should return the sum of multiple numbers', () => {
     expect(add('1,2,3,4')).toBe(10);
   });
+
+  test('handles newlines between numbers', () => {
+    expect(add("1\n2,3")).toBe(6);
+    expect(add("10\n20\n30")).toBe(60);
+});
+
 });
